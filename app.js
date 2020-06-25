773c8853157006853789f9bb509b0476ab3b3a1c
Sara Culhane <sculs@Saras-MacBook-Pro.local>
2020-06-25 15:17:56 -0400

    added changes

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const html = require('html-template-tag');
 const layout = require('./views/layout');
 const pg = require('pg')
 const Sequelize = require('sequelize');
-const { db } = require('./models');
+const models = require('./models');
 
 
 const app = express();
+
 
 
 app.use(morgan("dev"));
@@ -19,7 +20,14 @@ app.get('/', (req,res) => {
 
 const PORT = 1337;
 
+const tableCreate =  async () =>  {
+    await models.User.sync({ force: true})
+    await models.Page.sync({force : true})
+}
+
+
 app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
   });
   
+  tableCreate()
--- models/index.js
+++ models/index.js
@@ -1,6 +1,43 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
+
+// db.authenticate().
+// then(() => {
+//   console.log('connected to the database');
+// })
+
+const Page = db.define('page', {
+    title: {
+    type: Sequelize.STRING,
+    allowNull : false },
+    slug:{
+        type: Sequelize.STRING,
+        allowNull : false },
+ 
+    content: {
+        type: Sequelize.TEXT,
+        allowNull : false },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+
+});
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull : false },
+    email: {
+        type: Sequelize.STRING,
+        allowNull : false,
+        validate : {
+            isEmail : true
+        } 
+    }
+});
 
 module.exports = {
-  db
-}
+  db,Page, User
+};
