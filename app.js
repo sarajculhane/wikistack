2fda51ace9b84a35b436188e65a199d8d59837ec
Sara Culhane <sculs@Saras-MacBook-Pro.local>
2020-06-25 14:31:13 -0400

    commits

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const html = require('html-template-tag');
+const layout = require('./views/layout');
+const pg = require('pg')
+const Sequelize = require('sequelize');
+const { db } = require('./models');
+
 
 const app = express();
 
@@ -8,10 +14,10 @@ app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req,res) => {
-    res.send('hello world');
-})
+    res.send(layout('hi'));
+});
 
-const PORT = 1337
+const PORT = 1337;
 
 app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
